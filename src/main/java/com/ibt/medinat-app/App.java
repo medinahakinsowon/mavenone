9017f87856d9f05f84dc40e9635057001792d759
medinahakinsowon <medinahakinsowon@gmail.com>
2024-07-02 10:04:40 +0100

    all the files for the first time git add .git add .!

---
+++ src/main/java/com/ibt/medinat-app/App.java
@@ -0,0 +1,13 @@
+package com.ibt.medinat_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!: this is Medinat Akinsowon developer" );
+    }
+}
